a5507e61c5cc9311b7a0b301af5bd0bdfb8fbac1
Choi juhee <121925236+choidu@users.noreply.github.com>
2024-04-15 15:05:34 +0900

    Add files via upload

---
+++ choidu/index.js
@@ -0,0 +1,47 @@
+const clock = document.querySelector("h2#clock");
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
+
+const colors = [
+    "#ef5777",
+    "#575fcf",
+    "#4bcffa",
+    "#34e7e4",
+    "#0be881",
+    "#f53b57",
+    "#3c40c6",
+    "#0fbcf9",
+    "#00d8d6",
+    "#05c46b",
+    "#ffc048",
+    "#ffdd59",
+    "#ff5e57",
+    "#d2dae2",
+    "#485460",
+    "#ffa801",
+    "#ffd32a",
+    "#ff3f34"
+  ];
+  
+  const btnColor = document.querySelector("#randomcolor");
+  
+  function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)];
+  }
+  
+  function buttonClick() {
+    const firstColor = getRandomColor();
+    const secondColor = getRandomColor();
+    document.body.style.background = `linear-gradient(${firstColor}, ${secondColor})`;
+  }
+  
+  btnColor.addEventListener("click", buttonClick);
